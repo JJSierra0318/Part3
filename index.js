6419bc8ae5c5cf9b35b45db59c8cdabbb8af63da
JJSierra0318 <juanjosesierram@gmail.com>
2021-12-12 16:24:37 -0500

    3b

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(cors())
+
+app.use(express.static('build'))
 
 morgan.token('post', (request) => JSON.stringify(request.body))
 
